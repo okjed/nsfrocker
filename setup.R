6efd0af9f060155b8c04b28163258387d259ce2a
James Deaton <jdeaton@gmail.com>
2020-09-11 11:23:50 -0500

    adjusted packages

--- setup.R
+++ setup.R
@@ -1,4 +1,4 @@
 #list.of.packages <- c("urltools","igraph","visNetwork","ipapi","tidyverse","devtools","shinydashboard","DT","leaflet")
-list.of.packages <- c("urltools","igraph","visNetwork","tidyverse","devtools","shinydashboard","DT","leaflet")
+list.of.packages <- c("urltools","igraph","visNetwork","tidyverse","devtools","shinydashboard","DT","leaflet","ggmap","mapproj","maps")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
